2ee4e4713b9ef399c72c0893a9c5299ee6429650
Hyperion1040 <mateusz.worek@gmail.com>
2021-03-02 16:18:26 +0100

    paypal button added

--- steps.js
+++ steps.js
@@ -1060,4 +1060,90 @@ git commit -m "first commit"
 /* give address where git will push actual repository */
 git remote add origin https://github.com/MateuszWorek/react-phone-store.git
 /* push origin to repository */
-git push -u origin main
+git push -u origin master
+
+/* paypall button */
+npm install --save react-paypal-express-checkout
+
+
+/* Cart/PaypalButton.js */
+import React from 'react';
+import PaypalExpressBtn from 'react-paypal-express-checkout';
+
+export default class MyApp extends React.Component {
+    render() {
+        const onSuccess = (payment) => {
+            // Congratulation, it came here means everything's fine!
+            		console.log("The payment was succeeded!", payment);
+            		// You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
+        }
+
+        const onCancel = (data) => {
+            // User pressed "cancel" or close Paypal's popup!
+            console.log('The payment was cancelled!', data);
+            // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
+        }
+
+        const onError = (err) => {
+            // The main Paypal's script cannot be loaded or somethings block the loading of that script!
+            console.log("Error!", err);
+            // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
+            // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
+        }
+
+        let env = 'sandbox'; // you can set here to 'production' for production
+        let currency = 'USD'; // or you can set this value from your props or state
+        let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+        // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
+
+        const client = {
+            sandbox:    'YOUR-SANDBOX-APP-ID',
+            production: 'YOUR-PRODUCTION-APP-ID',
+        }
+        // In order to get production's app-ID, you will have to send your app to Paypal for approval first
+        // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):
+        //   => https://developer.paypal.com/docs/classic/lifecycle/sb_credentials/
+        // For production app-ID:
+        //   => https://developer.paypal.com/docs/classic/lifecycle/goingLive/
+
+        // NB. You can also have many Paypal express checkout buttons on page, just pass in the correct amount and they will work!
+        return (
+            <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
+        );
+    }
+}
+
+
+/* Cart.js */
+history={ this.props.history }
+
+/* CartTotals.js */
+import PayPalButton from './PaypalButton';
+
+, history
+
+<PayPalButton total={ cartTotal } clearCart={ clearCart } history={ history }></PayPalButton>
+
+/* PayPalButton.js */
+const client = {
+    sandbox:    'AXG_yhH4S9l2PwtOPUMYMsxaTPVwlelSYXpIu9qhF3DOAJGNdXeqRLAgQAoUyh5sM1G3aUGPKp-Qku2v',
+    production: 'YOUR-PRODUCTION-APP-ID',
+}
+
+/* PaypalButton.js */
+this.props.clearCart();
+this.props.history.push('/');
+
+// let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+
+this.props.total
+
+/* create env file for development */
+/* .env.development */
+REACT_APP_APP_ID=AXG_yhH4S9l2PwtOPUMYMsxaTPVwlelSYXpIu9qhF3DOAJGNdXeqRLAgQAoUyh5sM1G3aUGPKp-Qku2v
+
+/* .gitignore */
+.env.development
+
+/* PaypalButtons.js */
+process.env.REACT_APP_APP_ID
